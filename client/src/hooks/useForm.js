12b622473245884ff3984e7ee05b94afb37a9793
Lukecp <lukecp@pm.me>
2021-01-22 22:03:57 -0500

    added state to PlanList and custom useForm hook to CheckoutForm

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,11 @@
-// write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export const useForm = (initialValue) => {
+	const [value, setValue] = useState(initialValue);
+
+	const handleChanges = (e) => {
+		setValue(e.target.value);
+	}
+
+	return [value, setValue];
+};
